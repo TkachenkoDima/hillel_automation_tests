3cef2a817259a27a77a8585d7e5c09d4e8fadeb8
dimatkachenko <demitry.tkachenko@gmail.com>
2021-11-03 23:17:34 +0200

    Homework 19 wip

--- src/test/java/Homeworks/Homework19/BookingTest.java
+++ src/test/java/Homeworks/Homework19/BookingTest.java
@@ -2,11 +2,11 @@ package Homeworks.Homework19;
 
 import Homeworks.BaseTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.nio.file.WatchEvent;
 import java.util.List;
 
 public class BookingTest extends BaseTest {
@@ -14,17 +14,17 @@ public class BookingTest extends BaseTest {
     private final String checkOutButton = "div[data-mode='checkout']";
     private final String englishLanguageIcon = "[lang|=en-us]";
     private final String searchDestinationButton = "#ss";
-    private final String searchButton = "button[type='submit']";
+    private final String searchButton = "button[type='submit'";
     private final String monthsSelector = ".bui-calendar__wrapper";
     private final String monthSelector = ".bui-calendar__month";
     private final String daySelector = ".bui-calendar__date";
     private final String nextMonthIcon = ".bui-calendar__control--next";
-
     private final String destinationField = "input[placeholder]";
     private final String checkInDateField = "div[data-placeholder='Check-in Date']";
     private final String checkOutDateField = "div[data-placeholder='Check-out Date'";
+    private final String dateSelector = "div[class^=sb-searchbox__input]";
 
-    private final String reviewScore = "__bui-50";
+    private final String reviewScore = "div[data-filters-item='review_score:review_score=90']";
 
     private final String url = "https://www.booking.com";
     private final String destination = "London";
@@ -42,9 +42,9 @@ public class BookingTest extends BaseTest {
         driver.get(url);
         driver.findElement(By.xpath(languageIcon)).click();
         driver.findElement(By.cssSelector(englishLanguageIcon)).click();
-        driver.findElement(By.cssSelector(searchDestinationButton)).sendKeys(destination + Keys.ENTER);
+        driver.findElement(By.cssSelector(searchDestinationButton)).sendKeys(destination);
+        driver.findElement(By.cssSelector(dateSelector)).click();
         selectArrivalDate();
-        driver.findElement(By.cssSelector(checkOutButton)).click();
         selectDepartureDate();
         driver.findElement(By.cssSelector(searchButton)).click();
         Assert.assertEquals(driver.findElement(By.cssSelector(destinationField)).getAccessibleName(), destination,
@@ -56,15 +56,20 @@ public class BookingTest extends BaseTest {
                 departureWeekday + ", " + departureMonth + " " + departureDate + ", " + departureYear,
                 "Check out date is not as expected");
 
-        driver.findElement(By.id(reviewScore)).click();
+        driver.findElement(By.cssSelector(reviewScore)).click();
+
+        List<WebElement> score = driver.findElements(By.cssSelector("div[class^='hotellist_wrap']"));
+//        div[aria-label]
+
+        System.out.println("kek");
     }
 
     public void selectArrivalDate() {
-        List<WebElement> checkInMonths = driver.findElements(By.cssSelector(monthsSelector));
-        for (WebElement monthArrival : checkInMonths) {
+        List<WebElement> month = driver.findElements(By.cssSelector(monthsSelector));
+        for (WebElement monthArrival : month) {
             if (monthArrival.findElement(By.cssSelector(monthSelector)).getText().equals(arrivalMonth + " " + arrivalYear)) {
-                List<WebElement> checkInDays = monthArrival.findElements(By.cssSelector(daySelector));
-                for (WebElement arrivalDay : checkInDays) {
+                List<WebElement> days = monthArrival.findElements(By.cssSelector(daySelector));
+                for (WebElement arrivalDay : days) {
                     if (arrivalDay.getText().equals(arrivalDate)) {
                         arrivalDay.click();
                         break;
@@ -75,11 +80,11 @@ public class BookingTest extends BaseTest {
     }
 
     public void selectDepartureDate() {
-//        List<WebElement> checkOutMonths = driver.findElements(By.cssSelector(monthsSelector));
-//        for (WebElement departureMonth : checkOutMonths) {
-//            if (departureMonth.findElement(By.cssSelector(monthSelector)).getText().equals(departureYearAndMonth)) {
-                List<WebElement> checkOutDays = driver.findElements(By.cssSelector(daySelector));
-                for (WebElement departureDay : checkOutDays) {
+        List<WebElement> month = driver.findElements(By.cssSelector(monthsSelector));
+        for (WebElement monthDeparture : month) {
+            if (monthDeparture.findElement(By.cssSelector(monthSelector)).getText().equals(departureMonth + " " + departureYear)) {
+                List<WebElement> days = monthDeparture.findElements(By.cssSelector(daySelector));
+                for (WebElement departureDay : days) {
                     if (departureDay.getText().equals(departureDate)) {
                         departureDay.click();
                         break;
@@ -87,3 +92,5 @@ public class BookingTest extends BaseTest {
                 }
             }
         }
+    }
+}
